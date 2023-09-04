ba6af03751127ba90e49eb4a2949f61409cbc64d
swiftproger <swiftapp@mail.ru>
2023-09-05 00:35:51 +0300

    add method CalculationModel

--- Calculator/Calculator/Model/CalculationModel.swift
+++ Calculator/Calculator/Model/CalculationModel.swift
@@ -14,5 +14,39 @@ class CalculationModel {
     private var currentNumber = ""
     private var currentOperation = OperationModel.noAction
     
+    public func setNumber(number: Int) {
+        currentNumber.append(String(number))
+    }
+    
+    public func getCurrentNumber() -> String {
+        currentNumber
+    }
+    
+    public func setOperation(operation: OperationModel) {
+        guard let number = Double(currentNumber) else { return }
+        firstNumber = number
+        currentNumber = ""
+        currentOperation = operation
+    }
+    
+    public func getResult() -> String {
+        guard let number = Double(currentNumber) else { return "" }
+        secondNumber = number
+        
+        switch currentOperation {
+            
+        case .noAction:
+            print("noAction")
+        case .addition:
+            return String(firstNumber + secondNumber)
+        case .subtraction:
+            return String(firstNumber - secondNumber)
+        case .multiplication:
+            return String(firstNumber * secondNumber)
+        case .division:
+            return String(firstNumber / secondNumber)
+        }
+        return "0"
+    }
     
 }
