38deb4f78fe729df0bc01c0fbdb2e475ac93c61c
swiftproger <swiftapp@mail.ru>
2023-09-05 00:36:22 +0300

    refactor MainStackView

--- Calculator/Calculator/View/MainStackView.swift
+++ Calculator/Calculator/View/MainStackView.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 protocol MainStackViewProtocol: AnyObject {
-    func topNumberButton(tag: Int)
-    func topActionButton(tag: Int)
+    func tapNumberButton(tag: Int)
+    func tapActionButton(tag: Int)
 }
 
 class MainStackView: UIStackView {
@@ -51,7 +51,7 @@ class MainStackView: UIStackView {
     
     @objc
     private func keyboardButtonTapped(sender: UIButton) {
-        print(sender.tag)
+        sender.tag < 10 ? delegate?.tapNumberButton(tag: sender.tag) : delegate?.tapActionButton(tag: sender.tag)
     }
     
     private func setupMainStackView() {
@@ -78,15 +78,15 @@ class MainStackView: UIStackView {
         let button7 = createButton(title: "7", tag: 7, color: .gray)
         let button8 = createButton(title: "8", tag: 8, color: .gray)
         let button9 = createButton(title: "9", tag: 9, color: .gray)
-        let buttonMulty = createButton(title: "x", tag: 14, color: .orange)
-        let fourtStackView = UIStackView(subviews: [button7, button8, button9, buttonMulty])
+        let buttonMulti = createButton(title: "x", tag: 14, color: .orange)
+        let fourStackView = UIStackView(subviews: [button7, button8, button9, buttonMulti])
         
         let buttonAC = createButton(title: "AC", tag: 18, color: .darkGray)
         let buttonPlusMinus = createButton(title: "+/-", tag: 17, color: .darkGray)
         let buttonPercent = createButton(title: "%", tag: 16, color: .darkGray)
-        let buttonDevide = createButton(title: "/", tag: 15, color: .orange)
-        let fifthStackView = UIStackView(subviews: [buttonAC, buttonPlusMinus, buttonPercent, buttonDevide])
+        let buttonDivide = createButton(title: "/", tag: 15, color: .orange)
+        let fifthStackView = UIStackView(subviews: [buttonAC, buttonPlusMinus, buttonPercent, buttonDivide])
         
-        stackViewArray = [fifthStackView, fourtStackView, thirdStackView, secondStackView, firstStackView]
+        stackViewArray = [fifthStackView, fourStackView, thirdStackView, secondStackView, firstStackView]
     }
 }
